a6c70ad77ff7fdfbede06cb237f90779d41f2f6c
Franciscus Valentinus Ongkosianbhadra <fvalentinus@student.ciputra.ac.id>
2021-04-29 20:35:02 +0700

    konsultasi done

---
+++ public/konsultasiForm.js
@@ -0,0 +1,54 @@
+'use strict';
+
+//grab a form
+const form_konsultasi = document.querySelector('.form_konsultasi');
+
+//grab an input
+const name = form_konsultasi.querySelector('#name');
+const email = form_konsultasi.querySelector('#email');
+const phone = form_konsultasi.querySelector('#phone');
+const address = form_konsultasi.querySelector('#address');
+const konsultasi = form_konsultasi.querySelector('#konsultasi');
+const description = form_konsultasi.querySelector('#description');
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+    apiKey: "AIzaSyCOmlwNh81n3nw5S5NmoK2-vTegWLtWNVg",
+    authDomain: "servisor-fa4bf.firebaseapp.com",
+    databaseURL: "https://servisor-fa4bf-default-rtdb.firebaseio.com",
+    projectId: "servisor-fa4bf",
+    storageBucket: "servisor-fa4bf.appspot.com",
+    messagingSenderId: "722678778553",
+    appId: "1:722678778553:web:4fd79994047389fb702968",
+    measurementId: "G-4W2316QSJ1"
+};
+
+
+//create a functions to push
+function firebasePushKonsultasi(name, email, phone, address, konsultasi, description){
+    //prevents from braking
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+
+    var konsultasiRef = firebase.database().ref('konsultasi').push().set(
+        {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            address: address.value,
+            konsultasi: konsultasi.value,
+            description: description.value,
+        }
+    )
+}
+
+//push on form submit
+if (form_konsultasi){
+    form_konsultasi.addEventListener('submit', function(evt1){
+        evt1.preventDefault();
+        firebasePushKonsultasi(name, email, phone, address, konsultasi, description);
+        return alert("Permintaan Anda telah terkirim. Terima Kasih.")
+    })
+}
+
