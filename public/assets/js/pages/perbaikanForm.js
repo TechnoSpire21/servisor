088151f13189e6ee63797b755db7c5b123c73def
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-10-24 01:09:06 +0700

    fix perbaikan

--- public/assets/js/pages/perbaikanForm.js
+++ public/assets/js/pages/perbaikanForm.js
@@ -1,10 +1,13 @@
 'use strict';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
-import { collection, addDoc, doc, setDoc} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-storage.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
+// import { collection, addDoc, doc, setDoc} from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
+// import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-storage.js";
+// import { firebase } from "https://www.gstatic.com/firebasejs/5.0.4/firebase.js"
+
 
 //grab a form
+
 const form_perbaikan = document.querySelector('.form_perbaikan');
 
 //grab an input
@@ -16,7 +19,7 @@ const kendala = form_perbaikan.querySelector('#kendala');
 const description = form_perbaikan.querySelector('#description');
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseApp = initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyCOmlwNh81n3nw5S5NmoK2-vTegWLtWNVg",
     authDomain: "servisor-fa4bf.firebaseapp.com",
     databaseURL: "https://servisor-fa4bf-default-rtdb.firebaseio.com",
@@ -27,29 +30,49 @@ const firebaseApp = initializeApp({
     measurementId: "G-4W2316QSJ1"
 });
 
-const db = getFirestore(firebaseApp);
-const storage = getStorage(firebaseApp);
+var db = firebase.firestore();
+var storage = firebase.storage();
 
 //create a functions to push
-async function firebasePushPerbaikan(name, email, phone, address, kendala, description) {
+function firebasePushPerbaikan(name, email, phone, address, kendala, description) {
 
-    try {
-       const docRef = await addDoc(collection(db, "perbaikan"), {
-            name: name.value,
-            email: email.value,
-            phone: phone.value,
-            address: address.value,
-            kendala: kendala.value,
-            description: description.value,
-            bukti: "",
-            kunci: "",
-        });
-        var refId = docRef.id;
+    const result = db.collection("perbaikan").add({
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        address: address.value,
+        kendala: kendala.value,
+        description: description.value,
+        bukti: "",
+        kunci: "",
+    })
+    .then((docRef) => {
+        const refId = docRef.id;
         console.log("Document written with ID: ", refId);
-        return refId;
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
+        uploadImage(refId);
+        
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+    // try {
+    //    const docRef = await addDoc(collection(db, "perbaikan"), {
+            // name: name.value,
+            // email: email.value,
+            // phone: phone.value,
+            // address: address.value,
+            // kendala: kendala.value,
+            // description: description.value,
+            // bukti: "",
+            // kunci: "",
+    //     });
+        // var refId = docRef.id;
+    //     console.log("Document written with ID: ", refId);
+    //     return refId;
+    // } catch (e) {
+    //     console.error("Error adding document: ", e);
+    // }
 
     // var perbaikanRef = firebase.database().ref('perbaikan').push().set(
     //     {
@@ -64,28 +87,39 @@ async function firebasePushPerbaikan(name, email, phone, address, kendala, descr
 }
 
 function uploadImage(refId) {
-    
+    const ref = firebase.storage().ref();
     const file = document.querySelector('#files').files[0];
-    const namaFoto = new Date() + '-' + file.name
-    console.log(namaFoto);
+    const namaFoto = new Date() + '-' + file.name;
     const metadata = {
         contentType: file.type
     }
-    const imageRef = ref(storage, "userImg/"+namaFoto);
-    const task = uploadBytes(imageRef, file, metadata);
 
-    task.then(snapshot => getDownloadURL(ref(snapshot))).then(url => { console.log(url), saveImage(url, refId) })
+    const task = ref.child(namaFoto).put(file, metadata);
+
+    task.then(snapshot => snapshot.ref.getDownloadURL()).then(url => { console.log(url), saveImage(url, refId) });
+    
 }
 
-async function saveImage(url, refId){
-
-    // var key = "perba";
-    var perbaikanQuery = doc(db, "perbaikan/"+refId);
-    const query = {
+function saveImage(url, refId){
+    console.log(typeof url);
+    db.collection("perbaikan").doc(refId).set({
         bukti: url,
         kunci: refId,
-    }
-    setDoc(perbaikanQuery, query, {merge: true});
+    }, { merge: true })
+    .then((docRef) => {
+        console.log("Document Updated");
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+    
+    // var key = "perba";
+    // var perbaikanQuery = doc(db, "perbaikan/"+refId);
+    // const query = {
+        // bukti: url,
+        // kunci: refId,
+    // }
+    // setDoc(perbaikanQuery, query, {merge: true});
     // var query = firebase.database().ref('perbaikan').once('value').then(function(snapshot) {
     //     snapshot.forEach(function(childSnapshot) {
     //             key = childSnapshot.key;
@@ -112,9 +146,7 @@ function tambahBukti(url, key){
 if (form_perbaikan) {
     form_perbaikan.addEventListener('submit', function (evt1) {
         evt1.preventDefault();
-        var result = firebasePushPerbaikan(name, email, phone, address, kendala, description);
-        console.log("hasil:", result);
-        uploadImage(result);
+        firebasePushPerbaikan(name, email, phone, address, kendala, description);
         return alert("Permintaan Anda telah terkirim. Terima Kasih.")
     })
 }
